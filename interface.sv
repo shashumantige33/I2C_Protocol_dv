0542cea03eb59a496ed4a5ef2cf8d090fd791f78
Shashank Mantige <shashumantige33@gmail.com>
2026-02-09 10:05:53 +0530

    Add files via upload

---
+++ interface.sv
@@ -0,0 +1,14 @@
+interface i2c_if;
+  
+  logic clk;
+  logic rst;
+  logic newd;
+  logic op;   
+  logic [7:0] din;
+  logic [6:0] addr;
+  logic [7:0] dout;
+  logic  done;
+  logic busy, ack_err;  
+  
+endinterface
+  
